d60a7a34fa4e32b100221789fbfa8aa9fb8bcc0c
mpirun <mpirun@bic.mskcc>
2016-03-10 14:36:47 -0500

    SVN:r4645  modified abra parameters

--- abra_wrapper.pl
+++ abra_wrapper.pl
@@ -58,7 +58,7 @@ if(-d "$working"){
 
 ###`$standardParams->{submit} $standardParams->{job_name} $standardParams->{cpu} $standardParams->{mem} $standardParams->{cluster_out} $additionalParams $JAVA/java -Xms256m -Xmx60g -XX:-UseGCOverheadLimit -Djava.io.tmpdir=/scratch/$uID -jar $ABRA/abra.jar --in $inBams --out $outBams --ref $refSeq --bwa-ref $bwaRef --targets $targets --working $working --threads 24`;
 
-`$JAVA/java -Xms256m -Xmx60g -XX:-UseGCOverheadLimit -Djava.io.tmpdir=/scratch/$uID -jar $ABRA/abra.jar --in $inBams --out $outBams --ref $refSeq --bwa-ref $bwaRef --targets $targets --working $working --threads 12 > $log 2>&1`;
+`$JAVA/java -Xms256m -Xmx60g -XX:-UseGCOverheadLimit -Djava.io.tmpdir=/scratch/$uID -jar $ABRA/abra.jar --in $inBams --out $outBams --ref $refSeq --bwa-ref $bwaRef --targets $targets --working $working --threads 12 --mnf 5 --mbq 150 > $log 2>&1`;
 
 my $eadj = $? >> 8;
 exit($eadj);
